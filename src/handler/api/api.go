efa2057366aff251d4ae712c0b5f8b9e4b738b5c
研发1 <dev1@qq.com>
2019-02-26 17:59:56 +0800

    chi router

--- src/handler/api/api.go
+++ src/handler/api/api.go
@@ -1,1 +1,31 @@
 package api
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/chi/middleware"
+	"github.com/moocss/chi-webserver/src/service"
+)
+
+func (s service.Service) Handler() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	cors := cors.New(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"POST", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	r.Use(cors.Handler)
+
+	return r
+}
--- src/router/router.go
+++ src/router/router.go
@@ -3,84 +3,20 @@ package router
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
-	"github.com/moocss/chi-webserver/src/model"
-	"github.com/moocss/chi-webserver/src/pkg/version"
-	"github.com/moocss/chi-webserver/src/router/middleware"
-	"github.com/moocss/chi-webserver/src/service"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 
-	sdHandle "github.com/moocss/chi-webserver/src/handler/api/sd"
-	"github.com/moocss/chi-webserver/src/handler/api/users"
+
 )
 
-func rootHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"text": "Welcome to api app.",
-	})
-}
-
-func versionHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"source":  "https://github.com/go-impatient/go-webserver",
-		"version": version.Version.String(),
-	})
-}
-
-// NotFound creates a gin middleware for handling page not found.
-func NotFound() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		model.SendError(c)
-	}
-}
-
 // Load loads the services, middlewares, routes, handlers.
-func Load(s service.Service, middlewares ...gin.HandlerFunc) *gin.Engine {
-	// gin app
-	g := gin.New()
+func Load() *chi.Mux {
+	r := chi.NewRouter()
 
-	// 使用中间件.
-	g.Use(gin.Logger())
-	g.Use(gin.Recovery())
-	g.Use(middleware.Handler())
-	g.Use(middleware.NoCache)
-	g.Use(middleware.Options)
-	g.Use(middleware.Secure)
-	g.Use(middleware.Version)
-	g.Use(middlewares...)
+	r.Use(middleware.Logger)
 
-	// 404 Handler.
-	g.NoRoute(NotFound())
+	r.Mount("/api", api.NewRouter())
+	r.Mount("/web", web.NewRouter())
 
-	g.GET("/version", versionHandler)
-	g.GET("/", rootHandler)
-
-	// The health check handlers
-	svcd := g.Group("/sd")
-	{
-		svcd.GET("/health", sdHandle.HealthCheck)
-		svcd.GET("/disk", sdHandle.DiskCheck)
-		svcd.GET("/cpu", sdHandle.CPUCheck)
-		svcd.GET("/ram", sdHandle.RAMCheck)
-	}
-
-	// v1 group
-	user := g.Group("/api/v1/user")
-	{
-		// authentication
-		user.Use(middleware.Auth())
-		user.POST("", func(context *gin.Context) {})
-		user.DELETE("/:id", func(context *gin.Context) {})
-		user.PUT("/:id", func(context *gin.Context) {})
-		user.GET("", func(context *gin.Context) {})
-		user.GET("/:id", users.HandleFindById(s))
-			// user.GET("/:username", users.HandleFind(s))
-	}
-
-	// v2 group
-	v2Group := g.Group("/api/v2")
-	{
-		v2Group.GET("", func(context *gin.Context) {})
-	}
-
-	return g
+	return r
 }
